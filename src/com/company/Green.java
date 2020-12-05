933cf2ec8a05846bf124517b9309f394990aba4d
Michail-KP <mmickast@gmail.com>
2020-12-06 02:57:36 +0400

    Review

--- src/com/company/Green.java
+++ src/com/company/Green.java
@@ -1,5 +1,7 @@
 package com.company;
 
+// Если ты имел ввиду растение, то это не Green, а Plant
+// Этим наследованием ты говоришь, что у растения будет сила))
 public class Green extends ecoSystemCreature{
 
 }
