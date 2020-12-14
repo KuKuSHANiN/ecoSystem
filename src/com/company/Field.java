dce9989e3976911a2af6459b02326db4aaf2227f
D.SKY <dima.babenin@yandex.ru>
2020-12-14 14:31:08 +0400

    Trauble with Arrays initiation

--- src/com/company/Field.java
+++ src/com/company/Field.java
@@ -1,16 +1,31 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Field {
-    int i = 5;
-    String[][] field = {};
-    Animal[] cordinates = new Animal[i];
-    System in = new System();
+    int size = 5;
+    Scanner in = new Scanner(System.in);
 
     public void createAnimal() {
-        ArrayList<Animal> creature = new ArrayList<Animal>();
-        Animal coords = new Animal();
-        coords.getName() =
+        ArrayList<Animal> compareAnimalsPowerInZone = new ArrayList<>();   // создаем динамический массив куда будут
+                                                                                        //    заноситься все звери при встрече в точке.
+
+        Animal creature = new Animal();  // создаем зверя
+        creature.setName(in.nextLine());
+        creature.setPower(in.nextInt());
+        creature.setType(in.nextLine());
+        creature.setFood(in.nextInt());
+        compareAnimalsPowerInZone.add(creature);  //добавляем зверя в дин. массив
+
+        Animal [][] field = new Animal[5][5];  // пробовал и такое String [][] field = new String [5][5]
+
+
+    }
+
+    public void createStartAnimalInAllField(int size) {
+
     }
 }
+
+
